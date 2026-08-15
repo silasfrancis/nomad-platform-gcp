267998e79e37842e83b7521f19aa75fd23b5cfab
silasfrancis <silasfrancis07@gmail.com>
2026-08-16 00:16:32 +0100

    terraform fixes

--- terraform/compute/backend.tf
+++ terraform/compute/backend.tf
@@ -1,3 +1,15 @@
 terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.43.0"
+    }
+  }
+
   backend "gcs" {}
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
 }
